32e9c99c2a1bb55e50b26aff7ea32d7ab5871b80
Jabed Hasan <60073739+JabedHossainJabuu@users.noreply.github.com>
2023-08-21 00:20:55 +0600

    21-28-23 12.20 am

--- test.js
+++ test.js
@@ -1,23 +1,31 @@
-// ProjectCard.js
-import React from "react";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <link rel="icon" href="%PUBLIC_URL%/path-to-your-logo.ico" /> <!-- Update this line -->
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#000000" />
+  <meta
+    name="description"
+    content="Web site created using create-react-app"
+  />
+  <link rel="apple-touch-icon" href="%PUBLIC_URL%/path-to-your-logo.png" /> <!-- Update this line -->
 
-const ProjectCard = ({imgUrl, githubUrl }) => {
-  return (
-    <div className="project-card">
-      <a
-        href={githubUrl}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="project-card-image-link" // Apply any additional styling here
-      >
-        <img
-          src={imgUrl}
-          alt={imgUrl}
-          className="project-card-image" // Apply image-specific styling here
-        />
-      </a>
+  <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+
+  <title>Jabed's Portfolio</title>
+</head>
+<body>
+  <noscript>You need to enable JavaScript to run this app.</noscript>
+  <div id="root"></div>
+  
+  <!-- Navbar -->
+  <nav>
+    <div class="navbar-logo">
+      <img src="path-to-your-logo.png" alt="logo" class="logo" />
+      <span>Jabed's Portfolio</span>
     </div>
-  );
-};
-
-export default ProjectCard;
+  </nav>
+  <!-- Rest of your content -->
+</body>
+</html>
