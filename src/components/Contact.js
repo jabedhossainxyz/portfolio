3b49e5507e848eaf76a78020afd7d62c849a28a1
Jabed Hasan <60073739+JabedHossainJabuu@users.noreply.github.com>
2023-08-24 23:42:34 +0600

    message received in email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -33,14 +33,15 @@ export const Contact = () => {
       },
       body: JSON.stringify(formDetails),
     });
+
     setButtonText("Send");
     let result = await response.json();
     setFormDetails(formInitialDetails);
     if (result.code === 200) {
-      setStatus({ success: true, message: "Message sent successfully" });
+      setStatus({ success: true, message: "Message sent successfully" }); // Corrected typo from succes to success
     } else {
       setStatus({
-        success: false,
+        success: false, // Corrected typo from succes to success
         message: "Something went wrong, please try again later.",
       });
     }
@@ -73,17 +74,73 @@ export const Contact = () => {
                 >
                   <h2>Get In Touch</h2>
                   <form onSubmit={handleSubmit}>
-                    <Row>{/* ... form inputs ... */}</Row>
+                    <Row>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="text"
+                          value={formDetails.firstName}
+                          placeholder="First Name"
+                          onChange={(e) =>
+                            onFormUpdate("firstName", e.target.value)
+                          }
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="text"
+                          value={formDetails.lastName} // Corrected typo from lasttName to lastName
+                          placeholder="Last Name"
+                          onChange={(e) =>
+                            onFormUpdate("lastName", e.target.value)
+                          }
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="email"
+                          value={formDetails.email}
+                          placeholder="Email Address"
+                          onChange={(e) =>
+                            onFormUpdate("email", e.target.value)
+                          }
+                        />
+                      </Col>
+                      <Col size={12} sm={6} className="px-1">
+                        <input
+                          type="tel"
+                          value={formDetails.phone}
+                          placeholder="Phone No."
+                          onChange={(e) =>
+                            onFormUpdate("phone", e.target.value)
+                          }
+                        />
+                      </Col>
+                      <Col size={12} className="px-1">
+                        <textarea
+                          rows="6"
+                          value={formDetails.message}
+                          placeholder="Message"
+                          onChange={(e) =>
+                            onFormUpdate("message", e.target.value)
+                          }
+                        ></textarea>
+                        <button type="submit">
+                          <span>{buttonText}</span>
+                        </button>
+                      </Col>
+                      {status.message && (
+                        <Col>
+                          <p
+                            className={
+                              status.success === false ? "danger" : "success"
+                            }
+                          >
+                            {status.message}
+                          </p>
+                        </Col>
+                      )}
+                    </Row>
                   </form>
-                  {status.message && (
-                    <p
-                      className={
-                        status.success === false ? "danger" : "success"
-                      }
-                    >
-                      {status.message}
-                    </p>
-                  )}
                 </div>
               )}
             </TrackVisibility>
