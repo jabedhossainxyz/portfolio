e596c63595f040e212eab0548745d0bc5475cd7a
JabedHossainSwe <jabedhossain.swe@gmail.com>
2024-04-22 22:14:10 +0600

    Update Experience component: add company name links, format project lists
    The changes include:
    - Adding links to company names in the Experience component
    - Reformatting the project lists with better indentation and line breaks
    - Adding a

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -11,13 +11,23 @@ const Experience = () => {
               <div className="experience-details">
                 <div className="experience-item">
                   <h3>Position: Jr. PHP & Laravel Developer</h3>
-                  <p>Company Name: Leotech</p>
+                  <p>
+                    Company Name:{" "}
+                    <a
+                      href="https://leotechbd.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Leotech
+                    </a>
+                  </p>
                   <p>Location: Panthapath, Dhaka, Bangladesh</p>
                   <p>Duration: June 2023 - January 2024</p>
                   <ul>
                     <li>
-                      Developed and maintained web applications using HTML, CSS, Bootstrap, JavaScript, AJax for
-                      front-end and PHP for back-end.
+                      Developed and maintained web applications using HTML, CSS,
+                      Bootstrap, JavaScript, AJax for front-end and PHP for
+                      back-end.
                     </li>
                     <li>
                       Designed and implemented efficient database structures
@@ -32,29 +42,64 @@ const Experience = () => {
                       guidance to junior developers.
                     </li>
                   </ul>
-                  <h4>Projects:</h4>
+                  <h4>Projects I've worked with Leotech:</h4>
                   <ul>
-                    <li>Project 1: E-commerce Website</li>
+                    <li>
+                      Project 1:{" "}
+                      <a
+                        href="http://leomart.leotechbd.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{ textDecoration: "none" }}
+                      >
+                        E-commerce Website
+                      </a>
+                    </li>
                     <ul>
-                      <li>
-                        Developed this site using Laravel.
-                      </li>
+                      <li>Developed this site using Laravel.</li>
                       <li>
                         Implemented product catalog, cart, and checkout
                         functionalities.
                       </li>
                     </ul>
-                    <li>Project 2: CRM System</li>
+                    <br></br>
+                    <li>
+                      Project 2:{" "}
+                      <a
+                        href="https://www.leotechbd.com/school-management-software/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {" "}
+                        Education Management System
+                      </a>
+                    </li>
                     <ul>
-                      <li>Designed database schema using MySQL.</li>
+                      <li>Designed database schema using SQL.</li>
                       <li>
-                        Developed user interface using React and integrated with
-                        back-end APIs.
+                        Developed user interface using HTML, CSS, JavaScript,
+                        Bootstrap, Ajax, PHP and integrated with back-end APIs.
                       </li>
+                    </ul>
+                    <br></br>
+                    <li>
+                      Project 3: {""}{" "}
+                      <a
+                        href="http://dof.leotechbd.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Database Management System for Department of Fisheries,
+                        Matshya Bhaban, Bangladesh
+                      </a>
+                    </li>
+                    <ul>
+                      <li>This was a government project</li>
+                      <li>Developed user interface using Livewire.</li>
+                      <li>Designed database schema using MYSQL.</li>
                     </ul>
                   </ul>
                 </div>
-                {/* Add more experience items as needed */}
               </div>
             </div>
           </div>
