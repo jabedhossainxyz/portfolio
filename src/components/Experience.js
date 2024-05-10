3ce7fe9b55bf3095021e07314bc0ead0859d6599
JabedHossainSwe <jabedhossain.swe@gmail.com>
2024-05-10 23:30:37 +0600

    Add new experience entry for Laravel Developer position at It Bangla Limited
    This commit adds a new experience entry for a Laravel Developer position at It Bangla Limited. It includes the company name, location, duration, and a list

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -99,6 +99,96 @@ const Experience = () => {
                     </ul>
                   </ul>
                 </div>
+                <div className="experience-item">
+                  <h3>Position: Laravel Developer</h3>
+                  <p>
+                    Company Name:{" "}
+                    <a
+                      href="https://itbangla.org/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      It Bangla Limited
+                    </a>
+                  </p>
+                  <p>Location: Paltan, Dhaka, Bangladesh</p>
+                  <p>Duration: May 2024 - Present</p>
+                  <ul>
+                    <li>
+                      Developed and maintained web applications using HTML, CSS,
+                      Bootstrap, JavaScript, AJax for front-end and PHP for
+                      back-end.
+                    </li>
+                    <li>
+                      Designed and implemented efficient database structures
+                      using MySQL.
+                    </li>
+                    <li>
+                      Collaborated with cross-functional teams to deliver
+                      high-quality solutions tailored to client requirements.
+                    </li>
+                    <li>
+                      Contributed to code reviews and provided technical
+                      guidance to junior developers.
+                    </li>
+                  </ul>
+                  <h4>Projects I've worked with Leotech:</h4>
+                  <ul>
+                    <li>
+                      Project 1:{" "}
+                      <a
+                        href="http://leomart.leotechbd.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{ textDecoration: "none" }}
+                      >
+                        E-commerce Website
+                      </a>
+                    </li>
+                    <ul>
+                      <li>Developed this site using Laravel.</li>
+                      <li>
+                        Implemented product catalog, cart, and checkout
+                        functionalities.
+                      </li>
+                    </ul>
+                    <br></br>
+                    <li>
+                      Project 2:{" "}
+                      <a
+                        href="https://www.leotechbd.com/school-management-software/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Education Management System
+                      </a>
+                    </li>
+                    <ul>
+                      <li>Designed database schema using SQL.</li>
+                      <li>
+                        Developed user interface using HTML, CSS, JavaScript,
+                        Bootstrap, Ajax, PHP and integrated with back-end APIs.
+                      </li>
+                    </ul>
+                    <br></br>
+                    <li>
+                      Project 3: {""}
+                      <a
+                        href="http://dof.leotechbd.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Database Management System for Department of Fisheries,
+                        Matshya Bhaban, Bangladesh
+                      </a>
+                    </li>
+                    <ul>
+                      <li>This was a government project</li>
+                      <li>Developed user interface using Livewire.</li>
+                      <li>Designed database schema using MYSQL.</li>
+                    </ul>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
